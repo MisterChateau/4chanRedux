818050b21b90d6542d9a32731e119a51ef2eb906
chapi <jchateau@gmail.com>
2017-02-21 23:33:23 +0100

    Add board list route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const r = require('ramda');
 const timer = require('timers');
 var cors = require('cors')
 
+const log = r.tap(r.bind(console.log, console));
+
 const axios = require('axios').create({
     headers: { 'If-Modified-Since': (new Date()).toUTCString(), 'User-Agent': 'nodeChanClient/0.0.1'}
 });
@@ -34,6 +36,14 @@ app.get('/catalog/:board', function (req, res) {
         .catch(err => console.log(err));
 })
 
+const mapBoard = (board) => (console.log(board), { board: board.board, title: board.title });
+app.get('/boards', function (req, res) {
+    axios.get(`http://a.4cdn.org/boards.json`)
+        .then((response) => r.compose(r.map(mapBoard), r.path(['boards']))(response.data))
+        .then((boards) => res.send(boards))
+        .catch(err => console.log(err));
+})
+
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Server running on port 3000!')
 })
